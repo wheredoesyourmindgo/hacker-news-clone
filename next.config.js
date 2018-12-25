911ea0f5abbdd7b2af574f348cec608a85ba1d64
wheredoesyourmindgo <moemanchu@icloud.com>
2018-12-25 08:29:20 -0800

    Use Now v2 @now/next builder. Don't need @zeit/next-css in this project.

--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
-const withCSS = require('@zeit/next-css')
+const {PHASE_PRODUCTION_SERVER} =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants') // Get values from `next-server` package when building on now v2
 
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
-/* Without CSS Modules, with PostCSS */
-module.exports = withCSS({
-  webpack: (config) => {
-    config.plugins.push(
-      new SWPrecacheWebpackPlugin({
-        verbose: true,
-        minify: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        runtimeCaching: [
-          {
-            handler: 'networkFirst',
-            urlPattern: /^https?.*/
-          }
-        ]
-      })
-    )
-    return config
+module.exports = (phase, {defaultConfig}) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {}
   }
-})
 
-/* With CSS Modules */
-// module.exports = withCSS({ cssModules: true })
-
-/* With additional configuration on top of CSS Modules */
-/*
-module.exports = withCSS({
-  cssModules: true,
-  webpack: function (config) {
-    return config;
+  // ✅ Put the require call here.
+  // const withCSS = require('@zeit/next-css');
+  const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+  return {
+    webpack: (config) => {
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          verbose: true,
+          minify: true,
+          staticFileGlobsIgnorePatterns: [/\.next\//],
+          runtimeCaching: [
+            {
+              handler: 'networkFirst',
+              urlPattern: /^https?.*/
+            }
+          ]
+        })
+      )
+      return config
+    }
   }
-});
-*/
+}
